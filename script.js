0b5f162a3044ba62ffe586f2dbd6db21a434a0a8
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-07 15:54:22 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ const gerarTexto = document.querySelector('#criar-carta');
 const text = document.querySelector('#carta-texto');
 const paragraph = document.querySelector('#carta-gerada');
 
-function makeLetter() {
-  const splitWords = text.value.split(' ');
-  if (paragraph.innerHTML === '') {
-    for (let i = 0; i < splitWords.length; i += 1) {
+function createParagraph(splitWords) {
+  for (let i = 0; i < splitWords.length; i += 1) {
+    if (splitWords[i] !== '') {
       paragraph.innerHTML += `<span>${splitWords[i]}</span> `;
     }
-  } else {
-    paragraph.innerHTML = '';
-    makeLetter();
   }
 }
 
-gerarTexto.addEventListener('click', makeLetter);
+function makeLetter(splitWords) {
+  if (paragraph.innerHTML === '') {
+    createParagraph(splitWords);
+  } else {
+    paragraph.innerHTML = '';
+    makeLetter(splitWords);
+  }
+}
+
+function validateLetter() {
+  const checkEmpty = text.value.replace(/^\s+/, '').replace(/\s+$/, '');
+  const splitWords = checkEmpty.split(' ');
+  if (checkEmpty === '') {
+    paragraph.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    makeLetter(splitWords);
+  }
+}
+
+gerarTexto.addEventListener('click', validateLetter);
