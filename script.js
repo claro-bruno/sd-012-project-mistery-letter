1938ce1ee54118fb2c8d6d8de993b354c448ad0c
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-07 18:28:31 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -3,9 +3,48 @@ const text = document.querySelector('#carta-texto');
 const paragraph = document.querySelector('#carta-gerada');
 const contador = document.querySelector('#carta-contador');
 
+// Estilos do CSS
+const estilos = [
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['medium', 'big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
+
+function whileRoll() {
+  const quantidadeClasses = Math.ceil(Math.random() * 3 + 1);
+  const classPosition = [];
+  while (classPosition.length < quantidadeClasses) {
+    const random = Math.floor(Math.random() * 4);
+    if (!classPosition.includes(random)) {
+      classPosition.push(random);
+    }
+    if (classPosition.length === quantidadeClasses) {
+      break;
+    }
+  }
+  return classPosition;
+}
+
+function addClasses(item) {
+  const whichClass = whileRoll();
+  for (let i = 0; i < whichClass.length; i += 1) {
+    const styleSelection = estilos[i][Math.floor(Math.random() * estilos[i].length)];
+    item.classList.add(styleSelection);
+  }
+}
+
+function selectSpansCustomize() {
+  const allSpans = document.querySelectorAll('span');
+  for (let i = 0; i < allSpans.length; i += 1) {
+    addClasses(allSpans[i]);
+  }
+}
+
 function contarPalavras() {
   const spans = document.querySelectorAll('span');
   contador.innerHTML = spans.length;
+  selectSpansCustomize();
 }
 
 function createParagraph(splitWords) {
