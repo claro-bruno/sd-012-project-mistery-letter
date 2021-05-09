0fcd76c62a1ce580c6ec7f9b83d1141595170cb9
GMODias <gmd.delta@gmail.com>
2021-05-09 08:07:11 -0300

    [PT-BR]Refatorando. [EN] Refactoring.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const createLetterBtn = document.getElementById('criar-carta');
 const letterPlace = document.getElementById('carta-gerada');
 const letterText = document.getElementById('carta-texto');
 const arrayStyle = [['newspaper', 'magazine1', 'magazine2'], ['medium', 'big', 'reallybig'],
- ['rotateleft', 'rotateright'], ['skewleft', 'skewright']];
+  ['rotateleft', 'rotateright'], ['skewleft', 'skewright']];
 const letterCount = document.getElementById('carta-contador');
 
 // Checa se a carta está vazia ou cheia de espaços em brancos sem texto
@@ -23,43 +23,52 @@ function randomN(max) {
 function randomF(max) {
   return (Math.floor(Math.random() * max));
 }
-// Cria uma array forçando possuir pelo menos 1 elemento obrigatório
+// Cria uma array randomica forçando possuir pelo menos 1 elemento obrigatório
 function randomArray() {
-  for (let index = 0; index < 2; index += 0) {
-    let array = [randomN(1), randomN(1), randomN(1), randomN(1)];
+  while (true) {
+    const array = [randomN(1), randomN(1), randomN(1), randomN(1)];
     let t = 0;
     for (let index2 = 0; index2 < array.length; index2 += 1) {
       t += array[index2];
     }
-    if (t > 0) {return array }
+    if (t > 0) { return array; }
   }
 }
 
-// Gera saída de classes randomizadas - a randomização do tamanho é obrigatória
-function randomize() {
-  const word = document.getElementsByTagName('span');
-  for (let index = 0; index < (word.length); index += 1) {
-    const arrayWordstyle = randomArray();
-    for (let indexSt = 0; indexSt < (arrayWordstyle.length); indexSt += 1) {
-      if (arrayWordstyle[indexSt] !== 0) {
-        word[index].classList.add(arrayStyle[indexSt][randomF(arrayStyle[indexSt].length)])
-      }
+// Gera e acrescenta classes a todos os <spans> do documento de forma randômica
+function putRandomclass (elmnt) {
+  const arrayWordstyle = randomArray();
+  for (let index = 0; index < (arrayWordstyle.length); index += 1) {
+    if (arrayWordstyle[index] !== 0) {
+      elmnt.classList.add(arrayStyle[index][randomF(arrayStyle[index].length)]);
     }
   }
 }
-// Executa a ação de gerar a carta conforme os requisitos
-function createSpan() {
+
+function randomize() {
+  const word = document.getElementsByTagName('span');
+  for (let index = 0; index < (word.length); index += 1) {
+    putRandomclass(word[index]);
+  }
+}
+
+// Executa a ação de gerar a carta e contador conforme os requisitos
+function createSpan(elmnt,index) {
+  const word = elmnt[index].replace(/\s+/g, '');
+  if (word !== '') { letterPlace.innerHTML += `<span>${word}</span>`; }
+  if (index !== (elmnt.length - 1)) { letterPlace.innerHTML += ' '; }
+}
+
+function createLetter() {
   if (checkEmpty(letterText.value)) {
     letterPlace.innerText = '';
     const letterWords = letterText.value.split(' ');
     for (let index = 0; index < (letterWords.length); index += 1) {
-      const word = letterWords[index].replace(/\s+/g, '');
-      if (word !== '') { letterPlace.innerHTML += `<span>${word}</span>`; }
-      if (index !== (letterWords.length - 1)) { letterPlace.innerHTML += ' '; }
+      createSpan(letterWords, index);
     }
   }
   letterCount.innerText = letterPlace.innerText.split(' ').length;
   randomize();
 }
 
-createLetterBtn.addEventListener('click', createSpan);
+createLetterBtn.addEventListener('click', createLetter);
