0abfe73a2c738280890d636e25aed78bf3f05f37
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-09 13:19:03 -0300

    Feito requisito 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const content = document.getElementById('content');
+content.innerHTML = '';
+
+const cartaTexto = document.createElement('input');
+cartaTexto.id = 'carta-texto';
+
+content.appendChild(cartaTexto);
