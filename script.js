6008f3cebe2ccba92669ef11d676492a8c179ef0
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-09 15:25:54 -0300

    Requisitos 5, 6, 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function generateLetter() {
   const getInputText = document.getElementById('carta-texto');
   const getCreateLetter = document.getElementById('carta-gerada');
-  const wordsArray = getInputText.value.split(' ');
-  getCreateLetter.innerText = '';
-  for (let word of wordsArray) {
-    const newSpan = document.createElement('span');
-    newSpan.innerText = word;
-    getCreateLetter.appendChild(newSpan);
+  if (getInputText.value.match(/\S/g) !== null) {
+    getCreateLetter.innerText = '';
+    const wordsArray = getInputText.value.split(' ');
+    for (let word of wordsArray) {
+      const newSpan = document.createElement('span');
+      newSpan.innerText = word;
+      getCreateLetter.appendChild(newSpan);
+    } 
+  } else {
+    getCreateLetter.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
