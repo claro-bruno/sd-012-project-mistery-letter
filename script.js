d56ed27956a20cb70f0b51c100236ae6ba5cff82
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-09 13:49:34 -0300

    Feito requisito 2

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ content.innerHTML = '';
 
 const cartaTexto = document.createElement('input');
 cartaTexto.id = 'carta-texto';
-
 content.appendChild(cartaTexto);
+
+const cartaGerada = document.createElement('p');
+cartaGerada.id = 'carta-gerada';
+content.appendChild(cartaGerada);
