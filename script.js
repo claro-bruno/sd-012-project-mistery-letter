8686f27b6874d343656902a5fe781956c1468685
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-09 21:34:21 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -12,57 +12,55 @@ function generateLetter() {
   if (getInputText.value.match(/\S/g) !== null) {
     getCreateLetter.innerText = '';
     const wordsArray = getInputText.value.split(' ');
+    const getWordCount = document.getElementById('carta-contador');
+    getWordCount.innerText = wordsArray.length;
     for (let word of wordsArray) {
       const newSpan = document.createElement('span');
       newSpan.innerText = word;
       getCreateLetter.appendChild(newSpan);
       if(newSpan.innerText !== '') {
-
         spanArray.push(newSpan);
       }
     }
-    randomLetterStyles(spanArray); 
+    randomLetterStyles(spanArray);
+   
   } else {
     getCreateLetter.innerText = 'Por favor, digite o conteúdo da carta.';
   }
-  return spanArray;
 }
 
 function randomLetterStyles(spans) {
-  //Itera sobre todas as palavras do array
-  console.log(spans)
+  // let usedGroupsArray = [];
   for (let span of spans) {
-    // console.log(word);
-    // Define quantos grupos de estilos vou aplicar
     let randomNumberGroups = Math.floor(Math.random() * allGroups.length);
-    //Faz o loop dentro dos possíveis grupos de estilos
-    for (let index = 0; index <= randomNumberGroups; index += 1) {
+    for (let indexGroup = 0; indexGroup <= randomNumberGroups; indexGroup += 1) {
       let choseGroup = Math.floor(Math.random() * allGroups.length);
       let randomNumberStyle = Math.floor(Math.random() * allGroups[choseGroup].length);
-      
       span.classList.add(allGroups[choseGroup][randomNumberStyle]);
-
+      // if (checkUsedGroups(choseGroup, usedGroupsArray) === 'used') {
+      //   indexGroup = indexGroup - 1;
+      // } else {
+      //   span.classList.add(allGroups[choseGroup][randomNumberStyle]);
+      //   usedGroupsArray.push(choseGroup);
+      // }
     }
-
-
   }
+}
 
-
+function checkUsedGroups(group, usedGroups) {
+  for (let indexCheck = 0; indexCheck < usedGroups.length; indexCheck += 1) {
+    if (group === usedGroups[indexCheck]) return 'used';
+  }
 }
 
 function randomElementStyles(target) {
-
   let randomNumberGroups = Math.floor(Math.random() * allGroups.length);
-  //Faz o loop dentro dos possíveis grupos de estilos
   for (let index = 0; index <= randomNumberGroups; index += 1) {
     let choseGroup = Math.floor(Math.random() * allGroups.length);
     let randomNumberStyle = Math.floor(Math.random() * allGroups[choseGroup].length);
     target.className = '';
     target.classList.add(allGroups[choseGroup][randomNumberStyle]);
-
   }
-  
-
 }
 
 document.addEventListener('click', (event) => {
@@ -73,5 +71,4 @@ document.addEventListener('click', (event) => {
     const eventTarget = event.target;
     randomElementStyles(eventTarget);
   }
-
 });
