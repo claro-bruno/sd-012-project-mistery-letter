31019399ae9a8e416fee55722abc7c3139a87519
Patrick Dack <patrickdack89@gmail.com>
2021-05-11 00:52:48 -0300

    Requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('carta-texto');
+const buttonCreate = document.getElementById('criar-carta');
+const geneteLetter = document.getElementById('carta-gerada');
+
+let letterSplited;
+
+buttonCreate.addEventListener('click', () => {
+  letterSplited = input.value.split(' ');
+  for (let index = 0; index < letterSplited.length; index += 1) {
+    const createSpan = document.createElement('span');
+    createSpan.innerHTML = letterSplited[index] + ' ';
+    geneteLetter.appendChild(createSpan);
+    // console.log(letterSplited[index]);
+  }
+})
