dd11f16bb42d4a1158f7521279c38634b6f314d5
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-14 18:40:24 -0300

    adiciona botao

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const cartaTexto = document.querySelector('#carta-texto');
-const cartaGerada = document.querySelector('#carta-gerada');
+const inputCartaTexto = document.querySelector('#carta-texto');
+const paragrafoCartaGerada = document.querySelector('#carta-gerada');
+const botaoCriarCarta = document.querySelector('#criar-carta');
+
+function criarCarta() {
+  let trecho = document.createElement('span');
+  trecho.innerText = inputCartaTexto.value;
+  paragrafoCartaGerada.appendChild(trecho);
+}
+
+botaoCriarCarta.addEventListener('click', criarCarta);
