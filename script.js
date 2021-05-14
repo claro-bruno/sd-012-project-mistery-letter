41703b474897d7765274be4386ae9f578b424802
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-14 20:43:54 -0300

    separa palavras em diferentes spans

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const paragrafoCartaGerada = document.querySelector('#carta-gerada');
 const botaoCriarCarta = document.querySelector('#criar-carta');
 
 function criarCarta() {
-  let trecho = document.createElement('span');
-  trecho.innerText = inputCartaTexto.value;
-  paragrafoCartaGerada.appendChild(trecho);
+  let palavra = '';
+  for (let index = 0; index < inputCartaTexto.value.length; index += 1) {
+     if (index === (inputCartaTexto.value.length - 1)) {
+      palavra += inputCartaTexto.value[index];
+      let trecho = document.createElement('span');
+      trecho.innerText = palavra;
+      paragrafoCartaGerada.appendChild(trecho);
+      trecho.style.padding = '5px';
+      palavra = '';
+    } else if (inputCartaTexto.value[index] !== ' ') {
+      palavra += inputCartaTexto.value[index];
+    } else {
+      let trecho = document.createElement('span');
+      trecho.innerText = palavra;
+      paragrafoCartaGerada.appendChild(trecho);
+      trecho.style.padding = '5px';
+      palavra = '';
+    }
+  }
 }
 
 botaoCriarCarta.addEventListener('click', criarCarta);
