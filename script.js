39f676cc74813167c74a4142ddfa14315343004f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-02 16:50:53 -0300

    adicionando requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textInput = document.querySelector('#carta-texto');
 const textButton = document.querySelector('#criar-carta');
 const textLetter = document.querySelector('#carta-gerada');
+const textCounter = document.querySelector('#carta-contador');
 const styleClasses = ['newspaper', 'magazine1', 'magazine2'];
 const sizeClasses = ['medium', 'big', 'reallybig'];
 const rotateClasses = ['rotateleft', 'rotateright'];
@@ -10,23 +11,30 @@ function randomNumber2() {
   return Math.round(Math.random() * 2);
 }
 
+function addingClasses(element) {
+  element.classList
+    .add(
+      styleClasses[randomNumber2()],
+      sizeClasses[randomNumber2()],
+      rotateClasses[randomNumber2()],
+      skewClasses[randomNumber2()],
+    );
+}
+
 function generateLetter() {
   textButton.addEventListener('click', () => {
     textLetter.innerText = '';
     const inputValue = textInput.value.split(' ');
-    if (textInput.value === '' || inputValue[inputValue.length - 1] === '') {
+    if (textInput.value === '' || inputValue[0] === '') {
       textLetter.innerText = 'Por favor, digite o conteúdo da carta.';
       return;
     }
+    if (inputValue[inputValue.length - 1] === '') inputValue.pop();
+    textCounter.innerText = inputValue.length;
     inputValue.forEach((element) => {
       const novoSpan = document.createElement('span');
       novoSpan.innerText = element;
-      novoSpan.classList
-        .add(
-          styleClasses[randomNumber2()],
-          sizeClasses[randomNumber2()],
-          rotateClasses[randomNumber2()],
-          skewClasses[randomNumber2()]);
+      addingClasses(novoSpan);
       textLetter.appendChild(novoSpan);
     });
   });
